b553703cb8dfff812c8982342f1776d1f0ebd067
dingjingjing12138 <d15813717657@163.com>
2021-10-10 07:09:33 +0800

    merge test

--- src/main/java/com/dingjj/collection/diy/GitTest2.java
+++ src/main/java/com/dingjj/collection/diy/GitTest2.java
@@ -10,5 +10,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("test stash");
         System.out.println("finish feature");
+        System.out.println("merge test");
     }
 }
